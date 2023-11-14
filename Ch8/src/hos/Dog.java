0522986303b63ac1dc3dab5e4c3940c4dc964f5c
ckswn0125 <92292233+ckswn0125@users.noreply.github.com>
2023-11-14 16:32:33 +0900

    ch8

--- Ch8/src/hos/Dog.java
+++ Ch8/src/hos/Dog.java
@@ -1,5 +1,28 @@
 package hos;
 
-public class Dog {
+public class dog implements Pet {
+	
+	private double runSpeed;
+	private String ownerName;
+	
+	public dog(String ownerName, double speed) {
+		
+		this.ownerName = ownerName;
+		this.runSpeed=speed;
+	}
+	
+	public String OwnerName() {
+		return ownerName;
+	}
+	
+	public double RunSpeed() {
+		return runSpeed;
+		
+	}
+	
+	public void Cry() {
+		System.out.println("Mong Mong");
+	}
+	
 
 }
